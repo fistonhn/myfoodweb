36a96ab2e64c6be30bb7ef80f4c61729d4f39f3f
fiston.h <fiston.h@allerin.nl>
2023-04-17 13:07:29 +0200

    add production url

--- providers/apis/axios.ts
+++ providers/apis/axios.ts
@@ -1,6 +1,10 @@
 import Axios from "axios";
 
 
+// export const axios = Axios.create({
+//     baseURL: "http://localhost:3000/api"
+// })
+
 export const axios = Axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://myfoodweb.vercel.app/api/"
 })
