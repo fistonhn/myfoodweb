d420e10357a0b239ebca3e696961baea72bfad13
fistonhn <55746279+fistonhn@users.noreply.github.com>
2023-04-18 02:56:00 +0200

    Delete food-web-v2 directory

--- food-web-v2/providers/apis/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './menueApis.ts'
-export * from './user'
-
-export * from './contractor'
-export * from './category'
-
-export * from './wage'
--- food-web-v2/utils/updateDateWithHoursAndMinutes.ts
+++
@@ -1,9 +0,0 @@
-export const updateDateWithHourAndMinute = (date: Date, hour: number, minute: number) => {
-    const updatedDate = new Date(date); // Create a new date object to avoid modifying the original date
-
-    updatedDate.setHours(hour); // Set the hour value
-    updatedDate.setMinutes(minute); // Set the minute value
-    updatedDate.setSeconds(0); // Set the seconds value to zero
-
-    return updatedDate;
-}
