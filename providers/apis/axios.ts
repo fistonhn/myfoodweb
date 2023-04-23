29d9ca5d12947eeb146ffb097ac65e57230aea94
fiston.h <fiston.h@allerin.nl>
2023-04-23 03:09:46 +0200

    few redesign

--- providers/apis/axios.ts
+++ providers/apis/axios.ts
@@ -1,9 +1,9 @@
 import Axios from "axios";
 
-export const axios = Axios.create({
-    baseURL: "http://localhost:3000/api"
-})
-
 // export const axios = Axios.create({
-//     baseURL: "https://myfoodweb.vercel.app/api/"
+//     baseURL: "http://localhost:3000/api"
 // })
+
+export const axios = Axios.create({
+    baseURL: "https://myfoodweb.vercel.app/api/"
+})
