dc6b1e52762e3f786190d36b5e381bd111859142
fiston.h <fiston.h@allerin.nl>
2023-05-03 16:00:27 +0300

    fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,29 @@
 import { GetServerSideProps } from 'next'
 import { getSession, signOut, useSession } from 'next-auth/react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AccountMenu from './AccountMenu'
+import { getUserApi } from '@/providers/apis'
+
 interface IHeader {
     onlyLogo?: boolean
     className?: string
 }
 const Header = ({ onlyLogo, className }: IHeader) => {
+    const [userData, setuserData] = useState<string>()
     const { data: session } = useSession()
 
-    const searchAdminRole = session?.user.role?.filter((rl: any)=> rl.role==='admin')?.map((it: any)=> it.role)[0]
+    const getCurrentUser = async() => {
+        const fetchUserData = await getUserApi({email: session?.user.email})
+        const searchAdminRole = fetchUserData?.data?.user.role?.filter((rl: any)=> rl.role==='admin')?.map((it: any)=> it.role)[0]
+
+        setuserData(searchAdminRole)
+    }
+
+
+    useEffect(() => {
+        getCurrentUser()    
+    })
 
     return (
         <div className={className}>
@@ -44,7 +57,7 @@ const Header = ({ onlyLogo, className }: IHeader) => {
                         !onlyLogo &&
                         <>
                             {
-                                searchAdminRole === "admin" &&
+                                userData === "admin" &&
                                 <h1 onClick={() => {
                                     window.location.href = "/admin/adminpage"
                                 }} className='text-white hover:text-primary font-bold cursor-pointer'>Admin</h1>
@@ -57,15 +70,16 @@ const Header = ({ onlyLogo, className }: IHeader) => {
                             }} className='text-white hover:text-primary font-bold cursor-pointer'>Operation</h1>
 
                             {
-                                searchAdminRole === "admin" &&
+                                userData === "admin" ?
                               <h1>
                                 <AccountMenu />
-                              </h1>
-                            }
-                            {
-                                searchAdminRole !== "admin" &&
+                              </h1> : 
                                 <h1 className=' font-bold text-white hover:text-primary cursor-pointer' onClick={() => { signOut() }}>logout</h1>
                             }
+                            {/* {
+                                userData !== "admin" &&
+                                <h1 className=' font-bold text-white hover:text-primary cursor-pointer' onClick={() => { signOut() }}>logout</h1>
+                            } */}
                             
                         </>
                     }
